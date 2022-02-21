f74a0a10da07b0779af168e8265bd1b930f9a7c1
Supakorn Netsuwan <earth_killerdark@hotmail.com>
2022-02-21 16:48:21 +0700

    compro-midterm

--- tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-(int i = 0; i < sizeof(myarray) / sizeof(myarray[0]); i++)
-    // {
-    //     for(int j = 0; j < strlen(myarray[i]); j++)
-    //     {
-    //         printf("%c + ", myarray[i][j]);
-    //     }
-    //     printf("\n");
-    // }
--- two_dimensional.c
+++ two_dimensional.c
@@ -57,9 +57,9 @@ int main()
         for (int j = 0; j < 5; j++)
         {
             printf("%c + ", myarray[i][j]);
-        }   
+        }
         printf("\n");
     }
-    
+
     return 0;
 }
